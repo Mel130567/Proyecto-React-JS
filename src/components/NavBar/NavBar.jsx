e881c4db47a116a315e4764901743588e88f4a25
Melanie Benedito <mel130567@gmail.com>
2021-10-25 11:48:14 -0300

    Componente NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+  const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-info">
+            <a className="navbar-brand" href="#">Patitas pet-shop</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Inicio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Perros</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Gatos</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Servicios</a>
+                </li>
+                </ul>
+            </div>
+     </nav>
+    )
+}
+
+export default NavBar
