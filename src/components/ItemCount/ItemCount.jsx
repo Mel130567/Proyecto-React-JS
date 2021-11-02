b2347c979dd43f087df16d1f119f4354636f7344
Melanie Benedito <mel130567@gmail.com>
2021-11-02 11:42:59 -0300

    itemCount

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react"
+
+const ItemCount = ({stock, initial, onAdd}) => {
+
+    const [count, setCount] = useState(initial)
+    
+    const sumarProd = () =>{
+        if(count < stock){
+            setCount(count + 1)
+        }else{
+            alert(`Superaste el stock disponible. Stock = ${stock}`)
+        }
+    }
+    const restarProd = () =>{
+        if(count > initial){
+            setCount(count - 1)
+        }else{
+            alert("Error. No puede agregarse menos de 1 unidad al carrito")
+
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <button className="btn btn-info mx-2" onClick={sumarProd}>+</button>
+                <span>{count}</span>
+                <button className="btn btn-info mx-2" onClick={restarProd}>-</button>
+                <button className="btn btn-outline-info btn-light mx-3" onClick={onAdd}>Agregar al carrito</button>
+            </div>
+        </>
+        
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,10 @@
+import ItemCount from "../ItemCount/ItemCount"
+
 const ItemListContainer = ({greetings}) => {
     return (
-        <div className="bg-secondary">
+        <div className="itemListContainer">
             <p>{greetings}</p>
+            <ItemCount stock={5} initial={1} onAdd/>
         </div>
         
     )
