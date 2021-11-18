9f2f806debfbc9befdebf2a9e45b028b35c0125d
Melanie Benedito <mel130567@gmail.com>
2021-11-17 23:03:43 -0300

    item count/clase eventos

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
 
-const ItemCount = ({stock, initial, onAdd}) => {
+const ItemCount = ({stock, initial, func}) => {
 
     const [count, setCount] = useState(initial)
     
@@ -11,6 +11,7 @@ const ItemCount = ({stock, initial, onAdd}) => {
             alert(`Superaste el stock disponible. Stock = ${stock}`)
         }
     }
+
     const restarProd = () =>{
         if(count > initial){
             setCount(count - 1)
@@ -26,7 +27,9 @@ const ItemCount = ({stock, initial, onAdd}) => {
                 <button className="btn btn-info mx-2" onClick={restarProd}>-</button>
                 <span>{count}</span>
                 <button className="btn btn-info mx-2" onClick={sumarProd}>+</button>
-                <button className="btn btn-outline-info btn-light m-3" onClick={onAdd}>Agregar al carrito</button>
+                <button className="btn btn-outline-info btn-light m-3" onClick={func}>
+                        Agregar al Carrito
+                </button>
             </div>
         </>
         
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react"
 import ItemCount from "../ItemCount/ItemCount"
+import {Link} from 'react-router-dom';
 
 const ItemDetail = ({productos}) =>{
+
+    const[addToCart, setAddToCart] = useState(false)
+
+    const handleAddToCart = async (e) => {
+        setAddToCart(true)
+    }
+
     return(
         <>
             <div className="card" style={{width: "18rem"}} key={productos[0].id}>
                 <img src={productos[0].pictureUrl} className="card-img-top" alt="..."></img>
+                {addToCart ? 
+                <div className="card-body">
+                    <Link to = {"/cart"}>
+                        <button className="btn btn-primary">
+                            Finalizar Compra
+                        </button>
+                    </Link>
+                </div> :
                 <div className="card-body">
                     <h5 className="card-title">{productos[0].tittle}</h5>
                     <p className="card-text">{productos[0].description}</p>
                     <p className="card-text">{productos[0].price}</p>
-                    <ItemCount stock={5} initial={1} onAdd/>
+                    <ItemCount stock={5} initial={1} func={handleAddToCart}/>
                 </div>
+                }
             </div>
         </>
     )
