051c58e633b9c63efb024e49e848f4e6559f130d
Melanie Benedito <mel130567@gmail.com>
2021-12-20 00:46:57 -0300

    proyecto final 2

--- src/components/ModalCarrito/ModalCarrito.jsx
+++ src/components/ModalCarrito/ModalCarrito.jsx
@@ -1,5 +1,5 @@
 import Modal from 'react-bootstrap/Modal'
-import { useState, useContext} from 'react'
+import { useState, useContext, useEffect} from 'react'
 import firebase from "firebase";
 import {getFirestore} from "../../service/getFirestore";
 import { UseCartContext } from '../../Context/CartContext';
@@ -18,6 +18,16 @@ const ModalCarrito = () =>{
 
     const {cart, totalPrice} = UseCartContext()
 
+    useEffect(
+        () => {
+            if(idOrden != ''){
+                setMensajeId(true)
+                console.log("usefefect",idOrden)
+            }
+        },
+        [idOrden]
+    )
+
     const handleChange=(e)=>{
         setDatosForm(
             {
@@ -28,8 +38,8 @@ const ModalCarrito = () =>{
     }
 
     const generarOrden = async (e) =>{
-
         e.preventDefault()
+
         const orden = {}
 
         orden.date = firebase.firestore.Timestamp.fromDate(new Date())
@@ -45,14 +55,13 @@ const ModalCarrito = () =>{
 
         const dbQuery = getFirestore()
         await dbQuery.collection('ordenes').add(orden)
-        .then(resp => setIdOrden(console.log(resp.id)))
+        .then(resp => setIdOrden(resp.id))
         .catch(err => console.log('error', err))
         .finally(() => setDatosForm({
             nombre: '',
             tel: '',
             email: ''
-        }))
-        
+        }),)
     
     }
 
@@ -63,15 +72,15 @@ const ModalCarrito = () =>{
         </Modal.Header>
 
         <Modal.Body>
-            <form className="d-flex flex-column bd-highlight mb-3" onSubmit={generarOrden} >
+            <form className="d-flex flex-column bd-highlight mb-3">
                 <label>Nombre:</label>
                 <input type="text" name='nombre' value={datosForm.nombre} onChange={handleChange}/>
                 <label>Email</label>
                 <input type="text" name='email' value={datosForm.email} onChange={handleChange}/>
                 <label>Telefono</label>
                 <input type="text" name='tel' value={datosForm.tel} onChange={handleChange}/>
-                <button className="btn btn-outline-info btn-light m-3" onClick={() => setMensajeId(true)}>Enviar</button>
-                {mensajeId ? <div><p>Tu numero de compra es : {idOrden}</p></div> : <div>No ha generado un numero de compra</div>}
+                <button className="btn btn-outline-info btn-light m-3" onClick={generarOrden}>Enviar</button>
+                {mensajeId ? <div><p>¡Felicidades su pedido se ha realizado con exito! Tu numero de compra es : {idOrden}</p></div> : <div>No ha generado un numero de compra</div>}
             </form>
         </Modal.Body>
 
